07ac1b8b776ec8538beaa5a8906ea8fd41948c4e
XaverBernard <xaver.bernard@student.fhws.de>
2021-10-13 10:49:44 +0200

    movement toggel

--- OpenGL/src/Movement.cpp
+++ OpenGL/src/Movement.cpp
@@ -1,54 +1,111 @@
 #include "Movement.h"
 #include "Mvp.h"
 
-Movement::Movement(GLFWwindow* window)
-	:window(window), position(glm::vec3(0, 1, 4))
+glm::mat4 Movement::moveAlone()
 {
-	glfwGetWindowSize(window, &windowWidth, &windowHeight);
-	lastTime = glfwGetTime();
+	m_Position = glm::vec3(0, 1, 0) + glm::vec3(4 * cos(m_LastTime), 0, 4 * sin(m_LastTime));
+
+	glm::mat4 mvp = mvp::getMvp(m_InitialFoV, 4, 3, 1.0f, m_Position, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+	return mvp;
+}
+
+glm::mat4 Movement::moveAloneParty()
+{
+	m_Position = glm::vec3(0, 1, 3) + glm::vec3(4 * cos(m_LastTime) * 2, 1, 4 * sin(m_LastTime) * 2);
+
+	glm::mat4 mvp = mvp::getMvp(m_InitialFoV, 4, 3, 1.0f, m_Position, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+	return mvp;
+}
+
+glm::mat4 Movement::moveMouseAndKey()
+{
+	float deltaTime = float(glfwGetTime() - m_LastTime);
+	double xpos, ypos;
+
+	glfwGetCursorPos(window, &xpos, &ypos);
+	glfwSetCursorPos(window, m_WindowWidth / 2, m_WindowHeight / 2);
+
+	m_HorizontalAngle += m_MouseSpeed * deltaTime * float(m_WindowWidth / 2 - xpos);
+	m_VerticalAngle += m_MouseSpeed * deltaTime * float(m_WindowHeight / 2 - ypos);
+
+	glm::vec3 m_Direction(
+		cos(m_VerticalAngle) * sin(m_HorizontalAngle),
+		sin(m_VerticalAngle),
+		cos(m_VerticalAngle) * cos(m_HorizontalAngle));
+
+	glm::vec3 right(
+		sin(m_HorizontalAngle - 3.14f / 2.0f),
+		0,
+		cos(m_HorizontalAngle - 3.14f / 2.0f));
+
+	glm::vec3 up = glm::cross(right, m_Direction);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		m_Position += m_Direction * deltaTime * m_Speed;
+	}
+	// Move backward
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		m_Position -= m_Direction * deltaTime * m_Speed;
+	}
+	// Strafe right
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+		m_Position += right * deltaTime * m_Speed;
+	}
+	// Strafe left
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+		m_Position -= right * deltaTime * m_Speed;
+	}
+
+	glm::mat4 mvp = mvp::getMvp(m_InitialFoV, 4, 3, 1.0f, m_Position, m_Position + m_Direction, up);
+
+	return mvp;
+}
+
+Movement::Movement(GLFWwindow* window)
+	:window(window), m_Position(glm::vec3(0, 1, 4))
+{
+	glfwGetWindowSize(window, &m_WindowWidth, &m_WindowHeight);
+	m_LastTime = glfwGetTime();
 }
 
 glm::mat4 Movement::getMvp()
 {
-	float deltaTime = float(glfwGetTime() - lastTime);
-	double xpos, ypos;
-
-	glfwGetCursorPos(window, &xpos, &ypos);
-	glfwSetCursorPos(window, windowWidth / 2, windowHeight / 2);
-
-	horizontalAngle += mouseSpeed * deltaTime * float(windowWidth / 2 - xpos);
-	verticalAngle += mouseSpeed * deltaTime * float(windowHeight / 2 - ypos);
-
-	glm::vec3 direction(
-		cos(verticalAngle) * sin(horizontalAngle),
-		sin(verticalAngle),
-		cos(verticalAngle) * cos(horizontalAngle));
-
-	glm::vec3 right(
-		sin(horizontalAngle - 3.14f / 2.0f),
-		0,
-		cos(horizontalAngle - 3.14f / 2.0f));
-
-	glm::vec3 up = glm::cross(right, direction);
+	glm::mat4 mvp;
 	
 
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-		position += direction * deltaTime * speed;
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+	{
+		m_Mode = m_ModeMenu[0];
 	}
-	// Move backward
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-		position -= direction * deltaTime * speed;
+	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+	{
+		m_Mode = m_ModeMenu[1];
 	}
-	// Strafe right
-	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-		position += right * deltaTime * speed;
-	}
-	// Strafe left
-	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-		position -= right * deltaTime * speed;
+	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+	{
+		m_Position = glm::vec3(0, -1, 2);
+		m_Direction = glm::vec3(0, 0, -2);
+		m_Mode = m_ModeMenu[2];
 	}
 
-	glm::mat4 mvp = mvp::getMvp(initialFoV, 4, 3, 1.0f, position, position + direction, up);
-	lastTime = glfwGetTime();
+	switch (m_Mode)
+	{
+	case 'A':
+		mvp = moveAlone();
+		break;
+	case 'B':
+		mvp = moveAloneParty();
+		break;
+	case 'C':
+		mvp = moveMouseAndKey();
+		break;
+	default:
+		mvp = moveAlone();
+		break;
+	}
+
+	m_LastTime = glfwGetTime();
 	return mvp;
 }
--- OpenGL/src/Movement.h
+++ OpenGL/src/Movement.h
@@ -8,15 +8,22 @@ class Movement
 {
 private:
 	GLFWwindow* window;
-	glm::vec3 position;
+	glm::vec3 m_Position;
+	glm::vec3 m_Direction;
 
-	float horizontalAngle = 3.14f;
-	float verticalAngle = 0.0f;
-	float initialFoV = 45.0f;
-	float speed = 2.0f;
-	float mouseSpeed = 0.05f;
-	int windowWidth, windowHeight;
-	double lastTime;
+	float m_HorizontalAngle = 3.14f;
+	float m_VerticalAngle = 0.0f;
+	float m_InitialFoV = 45.0f;
+	float m_Speed = 2.0f;
+	float m_MouseSpeed = 0.05f;
+	int m_WindowWidth, m_WindowHeight;
+	double m_LastTime;
+	char m_ModeMenu[3] = {'A', 'B', 'C'};
+	char m_Mode;
+
+	glm::mat4 moveAlone();
+	glm::mat4 moveAloneParty();
+	glm::mat4 moveMouseAndKey();
 
 public:
 	Movement(GLFWwindow* window);
